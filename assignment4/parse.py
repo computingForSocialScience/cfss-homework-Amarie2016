2ceb116ecb63d08ceb2f606ba8703307ffac576b
Marie <amarie@uchicago.edu>
2015-02-05 13:27:50 -0600

    done, making sure it is in

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -18,8 +18,10 @@ def readCSV(filename):
 #print lines[1]
 ### enter your code below
 
-def get_avg_latlng(lines):
-	numEntries = float(len(lines))
+def get_avg_latlng(filename):
+	lines = readCSV(filename)
+	lines = lines[:10]
+	numEntries = 0 #float(len(lines)) - 1.0
 	latPos = 128
 	lngPos = 129
 
@@ -27,12 +29,13 @@ def get_avg_latlng(lines):
 	latSum = 0.0
 	lngSum = 0.0
 
-	for i in lines:
-		if i[latPos] != "":
-			lat = float(latPos)
+	for line in range(1,len(lines)):
+		i = lines[line]
+		if i[latPos] != "" and i[lngPos] != "":
+			numEntries = numEntries + 1
+			lat = float(i[latPos])
 			latSum = latSum + lat
-		if i[lngPos] != "":
-			lng = float(lngPos)
+			lng = float(i[lngPos])
 			lngSum = lngSum + lng
 
 	avgLat = latSum/numEntries
@@ -43,36 +46,44 @@ def zip_code_dict(lines):
 	#create dict of zips
 	zipCounts = {} 
 	allZips = [] 
-	for line in lines:
+
+	for project in lines:
 		projZipList = []
-		zipOrder = 26
-		#zip is every 6th, first zip is the 26th
-		while zipOrder < 131:
-			projZipList.append(line[zipOrder])
+		zipOrder = 28
+		#zip is every 6th, first zip is the 28th...
+		#get list of all zipcodes listed for project: THIS WORKS
+		while zipOrder < 127:
+			projZipList.append(project[zipOrder])
 			zipOrder = zipOrder + 6
-		
+	
 		alreadyFound = False
 
 		#go through all of the contracter zip codes on this project (in this line)
 		for projZip in projZipList: 
+			#make sure it is a real zip code
+			if len(projZip) < 5:
+					break
+			else:
+				projZip = projZip[0:5]
 			#compare each of the current project zips to the list of zips already in the dict
 			for zipcode in allZips:
 				#if you find the project Zip is already in the dictionary, add one to the value of the entry
 				if zipcode == projZip:
-					zipCounts[zipCode] = zipCounts[zipCode] + 1
+					zipCounts[zipcode] = zipCounts[zipcode] + 1
 					alreadyFound = True
 			#if you did not find 
 			if alreadyFound == False:
 				allZips.append(projZip)
 				zipCounts[projZip] = 1
+	del zipCounts["CONTR"]
 	return zipCounts
 
-def zip_code_barchart(zipRecords):
-	#zipRecords = zip_code_dict(lines)
+def zip_code_barchart(name):
+	lines = readCSV(name)
+	#lines = lines[:500]
+	zipRecords = zip_code_dict(lines)
 	zipCodes = []
 	zipCounts = []
-
-	print zipRecords
 
 	#go through each item in the dictionary and convert int two lists by appending to end (preserve order)
 	for code, count in zipRecords.items():
@@ -82,7 +93,10 @@ def zip_code_barchart(zipRecords):
 	#build bar chart
 	x = np.arange(len(zipCodes))
 	
-	plt.bar(x, zipCounts)
+	plt.bar(x, zipCounts) 
+	plt.title("Zipcodes of contracters")
+	plt.xlabel('Zipcodes')
+	plt.ylabel('Number of Contracters')
 	plt.xticks(x+0.5, zipCodes, rotation = 90)
 	plt.show() #will force plots to come uo
 	return 0
@@ -90,9 +104,11 @@ def zip_code_barchart(zipRecords):
 
 
 if __name__ == "__main__":
-	#lines = readCSV(sys.argv[1])
-	#print len(lines[1]) #output- 131 lines
-	#avgLoc = get_avg_latlng(lines)
-	#print avgLoc
-	testZips = {"zip0":1, "zip1":4}
-	zip_code_barchart(testZips)
+	choice = sys.argv[1]
+	fileName = sys.argv[2]
+	if choice == "latlong":
+		print get_avg_latlng(fileName)
+	elif choice == "hist":
+		print zip_code_barchart(fileName)
+	else:
+		print "invalid call, please give the chosen function and data set as arguments"
