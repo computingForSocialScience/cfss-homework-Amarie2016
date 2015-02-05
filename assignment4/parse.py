968d0832914880358a40ec9aef8af55d02f9a06f
Marie <amarie@uchicago.edu>
2015-02-04 21:22:30 -0600

    graphing function of 3.2 works

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -1,5 +1,7 @@
 import csv
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
 
 def readCSV(filename):
     '''Reads the CSV file `filename` and returns a list
@@ -37,11 +39,60 @@ def get_avg_latlng(lines):
 	avgLng = lngSum/numEntries
 	return (avgLat, avgLng)
 
+def zip_code_dict(lines):
+	#create dict of zips
+	zipCounts = {} 
+	allZips = [] 
+	for line in lines:
+		projZipList = []
+		zipOrder = 26
+		#zip is every 6th, first zip is the 26th
+		while zipOrder < 131:
+			projZipList.append(line[zipOrder])
+			zipOrder = zipOrder + 6
+		
+		alreadyFound = False
+
+		#go through all of the contracter zip codes on this project (in this line)
+		for projZip in projZipList: 
+			#compare each of the current project zips to the list of zips already in the dict
+			for zipcode in allZips:
+				#if you find the project Zip is already in the dictionary, add one to the value of the entry
+				if zipcode == projZip:
+					zipCounts[zipCode] = zipCounts[zipCode] + 1
+					alreadyFound = True
+			#if you did not find 
+			if alreadyFound == False:
+				allZips.append(projZip)
+				zipCounts[projZip] = 1
+	return zipCounts
+
+def zip_code_barchart(zipRecords):
+	#zipRecords = zip_code_dict(lines)
+	zipCodes = []
+	zipCounts = []
+
+	print zipRecords
+
+	#go through each item in the dictionary and convert int two lists by appending to end (preserve order)
+	for code, count in zipRecords.items():
+		zipCodes.append(code)
+		zipCounts.append(count)
+
+	#build bar chart
+	x = np.arange(len(zipCodes))
+	
+	plt.bar(x, zipCounts)
+	plt.xticks(x+0.5, zipCodes, rotation = 90)
+	plt.show() #will force plots to come uo
+	return 0
+
+
 
 if __name__ == "__main__":
-	lines = readCSV(sys.argv[1])
+	#lines = readCSV(sys.argv[1])
 	#print len(lines[1]) #output- 131 lines
-	#line = lines[1]
-	#print line[130]
-	avgLoc = get_avg_latlng(lines)
-	print avgLoc
+	#avgLoc = get_avg_latlng(lines)
+	#print avgLoc
+	testZips = {"zip0":1, "zip1":4}
+	zip_code_barchart(testZips)
