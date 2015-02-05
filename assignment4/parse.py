27712e1a7ac6577d4eafdaca35227b166a592749
Marie <amarie@uchicago.edu>
2015-02-04 19:32:10 -0600

    part 3.1 works

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -12,5 +12,36 @@ def readCSV(filename):
         lines = list(rdr)
     return(lines)
 
-
+#lines = readCSV('permits.csv')
+#print lines[1]
 ### enter your code below
+
+def get_avg_latlng(lines):
+	numEntries = float(len(lines))
+	latPos = 128
+	lngPos = 129
+
+	#sum lat and lng
+	latSum = 0.0
+	lngSum = 0.0
+
+	for i in lines:
+		if i[latPos] != "":
+			lat = float(latPos)
+			latSum = latSum + lat
+		if i[lngPos] != "":
+			lng = float(lngPos)
+			lngSum = lngSum + lng
+
+	avgLat = latSum/numEntries
+	avgLng = lngSum/numEntries
+	return (avgLat, avgLng)
+
+
+if __name__ == "__main__":
+	lines = readCSV(sys.argv[1])
+	#print len(lines[1]) #output- 131 lines
+	#line = lines[1]
+	#print line[130]
+	avgLoc = get_avg_latlng(lines)
+	print avgLoc
