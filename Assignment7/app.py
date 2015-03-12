7b1c577d08feae4bc8da782169471b2b977fc2ae
Marie <amarie@uchicago.edu>
2015-03-12 03:43:41 -0500

    it should stop changing its mind as to what the actual problem is. i hate this- who wants things online anyway?

--- Assignment7/app.py
+++ Assignment7/app.py
@@ -16,37 +16,6 @@ c = db.cursor()
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def make_index_resp():
-    # this function just renders templates/index.html when
-    # someone goes to http://127.0.0.1:5000/
-    return(render_template('index.html'))
-
-
-@app.route('/playlists/')
-def make_playlists_resp():
-    c.execute("SELECT * FROM playlists")
-    playlists = c.fetchall()
-    return render_template('playlists.html',playlists=playlists)
-
-
-@app.route('/playlist/<playlistId>')
-def make_playlist_resp(playlistId):
-    return render_template('playlist.html',songs=songs)
-
-
-@app.route('/addPlaylist/',methods=['GET','POST'])
-def add_playlist():
-    if request.method == 'GET':
-        # This code executes when someone visits the page.
-        return(render_template('addPlaylist.html'))
-    elif request.method == 'POST':
-        # this code executes when someone fills out the form
-        artistName = request.form['artistName']
-        # YOUR CODE HERE
-        return(redirect("/playlists/"))
-
 def createNewPlaylist(artist):
     makePlaylists = "CREATE TABLE IF NOT EXISTS playlists (ID INT PRIMARY KEY AUTO_INCREMENT, rootArtist VARCHAR(300)) ENGINE=MyISAM DEFAULT CHARSET=utf8"
     makeSongs = "CREATE TABLE IF NOT EXISTS songs (playlistId INT, songOrder INT, artistName VARCHAR(300), albumName VARCHAR(300), trackName VARCHAR(300)) ENGINE=MyISAM DEFAULT CHARSET=utf8"
@@ -91,7 +60,43 @@ def createNewPlaylist(artist):
     c.close()
     db.close()
 
+@app.route('/')
+def make_index_resp():
+    # this function just renders templates/index.html when
+    # someone goes to http://127.0.0.1:5000/
+    return(render_template('index.html'))
+
+
+@app.route('/playlists/')
+def make_playlists_resp():
+    c.execute("SELECT * FROM playlists")
+    playlists = c.fetchall()
+    return render_template('playlists.html',playlists=playlists)
+
+
+@app.route('/playlist/<playlistId>')
+def make_playlist_resp(playlistId):
+    c.execute("SELECT * FROM songs")
+    playlist = c.fetchall()
+    return render_template('playlist.html',songs=songs)
+
+
+@app.route('/addPlaylist/',methods=['GET','POST'])
+def add_playlist():
+    if request.method == 'GET':
+        # This code executes when someone visits the page.
+        return(render_template('addPlaylist.html'))
+    elif request.method == 'POST':
+        # this code executes when someone fills out the form
+        artistName = request.form['artistName']
+        # YOUR CODE HERE
+        return(redirect("/playlists/"))
+
+
+
 
 if __name__ == '__main__':
+    #createNewPlaylist("Coldplay")
+    #createNewPlaylist("Arcade Fire")
     app.debug=True
     app.run(port=5001)
